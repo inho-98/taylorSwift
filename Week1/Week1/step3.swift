72c354614c150d90b1396c40a9b2f8c1e81c49ce
lnhk98 <hozzang666@gmai.com>
2022-05-28 14:08:39 +0900

    step3 함수 기능 분리

--- Week1/Week1/step3.swift
+++ Week1/Week1/step3.swift
@@ -7,21 +7,34 @@
 
 import Foundation
 
-func makePepero(using body: String, and topping: String, height: Int, stickHeight: Int) {
+func makePepero(using ingredient: String, and topping: String, height: Int, stickHeight: Int) {
     let information = """
                          <정보>
                          길이: \(height)
-                         몸통: \(body)
+                         몸통: \(ingredient)
                          토핑: \(topping)
                          막대길이: \(stickHeight)
                          """
-    let stick = " | | "
+    
     print(information, terminator: "\n\n")
+    makePeperoBody(of: height, using: ingredient, and: topping)
+    makePeperoStick(of: stickHeight)
+}
+
+func makePartOfBody(using ingredient: String, and topping: String) {
+    print("\(topping)\(ingredient) ")
+    print(" \(ingredient)\(topping)")
+}
+
+func makePeperoBody(of height: Int, using ingredint: String, and topping: String) {
     for _ in 1...height/2 {
-        print("\(topping)\(body) ")
-        print(" \(body)\(topping)")
+        makePartOfBody(using: ingredint, and: topping)
     }
-    for _ in 1...stickHeight {
+}
+
+func makePeperoStick(of height: Int) {
+    for _ in 1...height {
+        let stick = " | | "
         print(stick)
     }
 }
